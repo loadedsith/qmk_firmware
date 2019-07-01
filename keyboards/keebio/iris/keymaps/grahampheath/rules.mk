90c76e11476df7b43b391c763437097d93450afb
Graham Heath <heathg@google.com>
2019-07-01 14:00:48 -0500

    alt layer dir keys clean up, remove top row customization.

--- keyboards/keebio/iris/keymaps/grahampheath/rules.mk
+++ keyboards/keebio/iris/keymaps/grahampheath/rules.mk
@@ -4,3 +4,4 @@ MOUSEKEY_ENABLE = no       # Mouse keys(+4700)
 UNICODE_ENABLE = yes
 RAW_ENABLE = no
 DYNAMIC_KEYMAP_ENABLE = no
+NKRO_ENABLE = yes            # Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
