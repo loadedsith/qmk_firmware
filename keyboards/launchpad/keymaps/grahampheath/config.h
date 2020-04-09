416b15b988f01e2742e1d0cae8bd2e31b109264c
Graham Heath <heathg@google.com>
2020-04-09 14:14:34 -0500

    Version issue? Fixed!

--- keyboards/launchpad/keymaps/grahampheath/config.h
+++ keyboards/launchpad/keymaps/grahampheath/config.h
@@ -1,11 +1,13 @@
 #pragma once
 
 /* Underlight Configuration */
+#undef RGB_DI_PIN
 #define RGB_DI_PIN F4
 // #define RGBLIGHT_ANIMATIONS
 #define RGBLIGHT_EFFECT_STATIC_GRADIENT
 #define RGBLIGHT_EFFECT_RAINBOW_SWIRL
 
+#undef RGBLED_NUM
 #define RGBLED_NUM 4  // Number of LEDs
 #define RGBLIGHT_HUE_STEP 10
 #define RGBLIGHT_SAT_STEP 17
